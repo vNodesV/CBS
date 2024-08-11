8f7b23d41b01d7f8945e0b3ec42a314ae51aefbd
vNodesV <dsebster@vnodes.cloud>
2024-08-11 16:02:11 -0400

    restruct

--- kAtch/config/node_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace these with the actual RPC addresses of your trusted node and local node
-EXT_NODE_RPC="https://rpc.testnet.elys.network"
-LOCAL_NODE_RPC="http://0.0.0.0:26657"
-DAEMON="elysd"
-DAEMON_DIR=".elys"
--- kAtch/func/chain_info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Function to get the chain name from the external node
-get_chain_name() {
-  curl -s $EXT_NODE_RPC/genesis | jq -r '.result.genesis.chain_id' 2>/dev/null
-}
