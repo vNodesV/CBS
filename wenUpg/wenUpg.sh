6a15bcd1d00a91b91538afad1d0241083de9ed6e
vNodesV <dsebster@vnodes.cloud>
2025-01-13 09:45:13 -0500

    1.2

--- wenUpg/wenUpg.sh
+++ wenUpg/wenUpg.sh
@@ -64,13 +64,9 @@ while [[ $blocks_remaining -gt 0 ]]; do
   eta_seconds_remaining=$(echo "$eta_seconds % 60" | bc)
 
   # Display real-time countdown
-  LC_NUMERIC=C
-  printf "\rTarget Height: %d | Current Block Time: %.2f secs | Blocks Remaining: %d | ETA: %02dh %02dm %02ds" \
+LC_NUMERIC=C
+printf "\rTarget Height: %d | Current Block Time: %.2f secs | Blocks Remaining: %d | ETA: %02dh %02dm %02ds" \
   $upgrade_height $(printf "%.2f" $avg_block_time) $blocks_remaining $eta_hours $eta_minutes $eta_seconds_remaining
-
-
-
-
 
   # Wait for one second before updating
   sleep 1
